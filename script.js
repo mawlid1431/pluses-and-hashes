a245cff3f2bdcbbc9e0aca8a33a008d3460daf14
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-04 15:35:40 +0300

    Plus -&-

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Good Luck 💪🏾
+function plusHashCount(str) {
+  let hashCount = 0;
+  let plusCount = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '#') {
+      hashCount++;
+    } else if (str[i] === '+') {
+      plusCount++;
+    }
+  }
+
+  return [hashCount, plusCount];
+}
+
+// Example usages:
+console.log(plusHashCount("###+")); // Output: [1, 3]
+console.log(plusHashCount("##+++#")); // Output: [3, 3]
+console.log(plusHashCount("#+++#+#++#")); // Output: [6, 4]
+console.log(plusHashCount("")); // Output: [0, 0]
